3573b2d0c0ef35b23ca59b7522f3e45f2b5fed99
sagarrajkpatil <sagarrajkpatil@gmail.com>
2025-05-13 20:57:54 +0530

    Add files via upload

---
+++ app/(site)/page.tsx
@@ -0,0 +1,40 @@
+import { Metadata } from "next";
+import Hero from "@/components/Hero";
+import Brands from "@/components/Brands";
+import Feature from "@/components/Features";
+import About from "@/components/About";
+import FeaturesTab from "@/components/FeaturesTab";
+import FunFact from "@/components/FunFact";
+import Integration from "@/components/Integration";
+import CTA from "@/components/CTA";
+import FAQ from "@/components/FAQ";
+import Pricing from "@/components/Pricing";
+import Contact from "@/components/Contact";
+import Blog from "@/components/Blog";
+import Testimonial from "@/components/Testimonial";
+import {dataConstant} from '@/components/DataConstant';
+
+export const metadata: Metadata = {
+  title: dataConstant.title,
+  description: dataConstant.description
+};
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <Brands />
+      <Feature />
+      <About />
+      <FeaturesTab />
+      <FunFact />
+      <Integration />
+      <CTA />
+      <FAQ />
+      <Testimonial />
+      <Pricing />
+      <Contact />
+      <Blog />
+    </main>
+  );
+}
---
+++ components/DataConstant/index.js
@@ -0,0 +1,16 @@
+export const dataConstant = {
+
+        title: "Sigma Industry",
+        description: "",
+        subtitle: "",
+
+
+        bodyTitle: "WELCOME TO OUR COMPANY",
+        bodySubTitle: "About Capital Bolts And Hardwares",
+        bodyText: "Capital Bolts & Hardware's is a ISO and CE & IATF 1649:2016 certified Fasteners Manufacturer well-known name among the topmost suppliers and manufacturers of premium quality fasteners. It was founded by Mr. Ramprakash Nagpal in the year 1959, to meet the growing demand for quality products for fixings' applications in the construction works, mechanical equipment, and household appliances. Over the past six decades, Capital Bolts & Hardwares has grown under the energetic leadership of 3 generations and evolved as a large scale company of premium fastening products. Quality checks and activity control at each stage are followed as a custom to ensure to deliver value to the customer. Capital bolts and hardware's is ISO certified and CE certified and IATF Certified Fasteners Manufacturer company.",
+        list: `<ul><li className="mt-5 text-black dark:text-white">
+                The Leading supplier and manufacturer in the world of Fasteners. </li>
+                <li className="mt-5 text-black dark:text-white">Quality, Product Standard and Service commitment.</li>
+                <li className="mt-5 text-black dark:text-white">Known for a wide product range availability</li> </ul>`
+
+    }
