24d5515fc207967412b14d586cf652eae4e46e10
Sagar Patil <sagarp@perscitussln.com>
2025-05-26 14:52:15 +0530

    set data

--- app/(site)/products/[id]/page.tsx
+++ app/(site)/products/[id]/page.tsx
@@ -1,35 +1,19 @@
-"use client";
-
 import BlogData from "@/components/Blog/blogData";
 import BlogItem from "@/components/Blog/BlogItem";
-import { Metadata } from "next";
-import { notFound } from "next/navigation";
-
-// export const metadata: Metadata = {
-//   title: "Sigma Industry",
-//   description: "Sigma Industry",
-// };
-
 
 const BlogPage = ({ params }: { params: { id: string } }) => {
-
-
-  console.log(params.id)
+  // console.log(params.id); // this will run on the server, not browser
 
   return (
-    <>
-      {/* <!-- ===== Blog Grid Start ===== --> */}
-      <section className="py-20 lg:py-25 xl:py-30">
-        <div className="mx-auto mt-15 max-w-c-1280 px-4 md:px-8 xl:mt-20 xl:px-0">
-          <div className="grid grid-cols-1 gap-7.5 md:grid-cols-2 lg:grid-cols-3 xl:gap-10">
-            {BlogData.map((post, key) => (
-              <BlogItem key={key} blog={post} />
-            ))}
-          </div>
+    <section className="py-20 lg:py-25 xl:py-30">
+      <div className="mx-auto mt-15 max-w-c-1280 px-4 md:px-8 xl:mt-20 xl:px-0">
+        <div className="grid grid-cols-1 gap-7.5 md:grid-cols-2 lg:grid-cols-3 xl:gap-10">
+          {BlogData.map((post, key) => (
+            <BlogItem key={key} blog={post} />
+          ))}
         </div>
-      </section>
-      {/* <!-- ===== Blog Grid End ===== --> */}
-    </>
+      </div>
+    </section>
   );
 };
 
